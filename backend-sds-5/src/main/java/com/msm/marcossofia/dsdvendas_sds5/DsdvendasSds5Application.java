54592737521ad6c1d6077d107a73bf14c9a35ac2
Marcos Santos <marcos1970moreirasantos@gmail.com.br>
2021-11-02 18:24:14 -0300

    Projetos iniciados

---
+++ backend-sds-5/src/main/java/com/msm/marcossofia/dsdvendas_sds5/DsdvendasSds5Application.java
@@ -0,0 +1,13 @@
+package com.msm.marcossofia.dsdvendas_sds5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdvendasSds5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdvendasSds5Application.class, args);
+	}
+
+}
---
+++ backend-sds-5/src/test/java/com/msm/marcossofia/dsdvendas_sds5/DsdvendasSds5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msm.marcossofia.dsdvendas_sds5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdvendasSds5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
