ad2177571924d9d41ee9f1ab245b013bbdb6b2e7
Marcos Santos <marcos1970moreirasantos@gmail.com.br>
2021-11-04 00:48:28 -0300

    Corregir nome

--- frontend-sds-5/src/componentes/footer/index.tsx
+++ frontend-sds-5/src/componentes/footer/index.tsx
@@ -2,7 +2,7 @@ const Footer =() =>{
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/marcos1970moreirasantos" target="_blank" rel="noreferrer">Marcos M dos santos</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/marcos1970moreirasantos" target="_blank" rel="noreferrer">Marcos M dos Santos</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
