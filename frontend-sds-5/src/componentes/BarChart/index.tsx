2a39be67da65333579f31d3700b991f0752d6bf9
Marcos Santos <marcos1970moreirasantos@gmail.com.br>
2021-11-20 20:37:08 -0300

    Reparos

--- frontend-sds-5/src/componentes/BarChart/index.tsx
+++ frontend-sds-5/src/componentes/BarChart/index.tsx
@@ -40,13 +40,12 @@ const BarChart = () => {
         axios.get(BASE_URL + '/sales/success-by-seller')
             .then(response => {
                 const data = response.data as SaleSuccess[];
-                const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
 
                 setchartData({
 
                     labels: {
-                        categories: myLabels
+                        categories: []
                     },
                     series: [
                         {
