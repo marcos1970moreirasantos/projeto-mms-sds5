d4e4270b9116c130b65b9fc01340e06193543e1d
Marcos Santos <marcos1970moreirasantos@gmail.com.br>
2021-11-29 06:57:36 -0300

    Verificado-4

--- frontend-sds-5/src/utils/request.ts
+++ frontend-sds-5/src/utils/request.ts
@@ -1,9 +1,1 @@
-import axios from "axios";
-
-const URL =
- process.env.REACT_APP_URL_BACKENDSDS5 ?? process.env.REACT_APP_BASE_URL;
-
- export const api = axios.create({
-     baseURL : `${ URL }`,
-
- });
+export const BASE_URL = process.env.REACT_APP_BACKENDSDS5_URL ?? 'http://localhost:8080';
